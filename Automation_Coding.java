ec51fccb0bcae14ff57b2f59bd98e5f9145e1136
ArshAzamKhan <arsh.khan26@yahoo.com>
2025-11-10 00:18:32 +0530

    Update in New branch

--- Automation_Coding.java
+++ Automation_Coding.java
@@ -1,5 +1,9 @@
 public class HelloWorld{
     public static void main(String[] args){
         System.out.println("Hello World");
+        int i =10;
+        for (int j=0; j<i; j++){
+            System.out.println(j);
+        }
     }
 }
