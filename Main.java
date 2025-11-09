f096d69e5878ee73b12718d6cc8be727f2a0882c
ArshAzamKhan <arsh.khan26@yahoo.com>
2025-11-10 01:36:30 +0530

    Star Program

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        int i =5;
+        for (int j=1; j<=i; j++){
+            for (int k =0; k<j; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
